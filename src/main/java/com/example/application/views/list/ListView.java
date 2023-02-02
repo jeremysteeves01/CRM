11dc493e288f8c411369dd7c0fa7940e664e36e4
jeremysteeves01 <jsteeves01@gmail.com>
2023-02-02 12:44:45 -0500

    commit

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,30 +1,50 @@
 package com.example.application.views.list;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.example.application.data.entity.Contact;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@PageTitle("list")
+@PageTitle("Contacts | Jeremy's CRM")
 @Route(value = "")
 public class ListView extends VerticalLayout {
-
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filterText = new TextField();
     public ListView() {
-        setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
+        addClassName("list-view");
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+
+        configureGrid();
+        add(
+                getToolbar(),
+                grid
+        );
+    }
+
+    private Component getToolbar() {
+        filterText.setPlaceholder("Filter by name ...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);//will wait for user to stop typing before we call the database. This prevents calling db on each keystroke.
+
+        Button addContactButton = new Button("Add Contact");
+        HorizontalLayout toolbar = new HorizontalLayout(filterText,addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName","lastName","email");
+        grid.addColumn(contact -> contact.getStatus().getName()).setHeader("Status");
+        grid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
+        grid.getColumns().forEach(col->col.setAutoWidth(true));
     }
 
 }
