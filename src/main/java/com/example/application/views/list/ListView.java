d9a56502da5c0f0d8120838c57487fd91eea8236
jeremysteeves01 <jsteeves01@gmail.com>
2023-02-02 13:04:12 -0500

    create ContactForm
    - created a new component ContactForm
    - added form field type ContactForm to ListView
    - created new method to create the forms by instantiating 2 lists of ComboBoxs

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -11,20 +11,38 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import java.util.Collections;
+
 @PageTitle("Contacts | Jeremy's CRM")
 @Route(value = "")
 public class ListView extends VerticalLayout {
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
+    ContactForm form;
     public ListView() {
         addClassName("list-view");
         setSizeFull();
 
         configureGrid();
+        configureForm();
         add(
                 getToolbar(),
-                grid
+                getContent()
         );
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid, form);
+        content.setFlexGrow(2,grid);
+        content.setFlexGrow(1,form);
+        content.addClassName("content");
+        content.setSizeFull();
+        return content;
+    }
+
+    private void configureForm() {
+        form = new ContactForm(Collections.emptyList(),Collections.emptyList());
+        form.setWidth("25em");
     }
 
     private Component getToolbar() {
